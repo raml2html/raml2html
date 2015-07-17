d424aefa121c5cfa541674035b81c003d337129b
Kevin Renskers <info@mixedcase.nl>
2015-07-17 22:18:23 +0000

    Render description for securityScheme

--- index.js
+++ index.js
@@ -71,6 +71,11 @@ function getDefaultConfig(mainTemplate, templatesPath) {
       var env = nunjucks.configure(templatesPath, {watch: false});
       markdown.register(env, marked);
 
+      // Add extra function for finding a security scheme by name
+      ramlObj.securitySchemeWithName = function(name) {
+        return ramlObj.securitySchemes[0][name];
+      };
+
       // Render the main template using the raml object and fix the double quotes
       var html = env.render(mainTemplate, ramlObj);
       html = html.replace(/&quot;/g, '"');
