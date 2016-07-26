f8190e681e3487a8d98f98cd7e55ffaad5a9f6ee
Daichi Ueura <daichi.ueura@gmail.com>
2016-07-26 22:44:55 +0900

    Improve multiple security schemes handling
    If there is more than one security scheme,
    the generated html doesn't describe all of
    them properly.

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function getDefaultConfig(mainTemplate, templatesPath) {
       // Add extra function for finding a security scheme by name
       ramlObj.securitySchemeWithName = function (name) {
         for (var index = 0; index < ramlObj.securitySchemes.length; ++index) {
-          if (ramlObj.securitySchemes[index][name] !== null) {
+          if (ramlObj.securitySchemes[index][name]) {
             return ramlObj.securitySchemes[index][name];
           }
         }
